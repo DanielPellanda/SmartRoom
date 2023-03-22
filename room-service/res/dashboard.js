84086412f2e4fbe6ac608e7444e36e8bf89cce8b
DanielPellanda <pellanda.daniel01@gmail.com>
2023-03-22 20:38:32 +0100

    Updated web app resources

--- room-service/res/dashboard.js
+++ room-service/res/dashboard.js
@@ -1,21 +1,69 @@
-let id_left = "pleft";
-let id_right = "pright"
-let id_value = "pvalue";
-let id_led = "pled";
-let id_bar = "pbar";
-let id_time = "time";
-let id_status = "status";
+const status_auto = 0;
+const status_dash = 1;
+const status_mobile = 2;
+
+let id_left = "extLeft";
+let id_right = "extRight"
+let id_value = "extValue";
+let id_led = "extLed";
+let id_bar = "extBar";
+let id_time = "lblTime";
+let id_status = "lblStatus";
 let id_graph = "lightChart";
+let id_switch = "chkLight";
+let id_roll = "nmbRoll";
+let id_button = "btnApply";
+let id_btn_release = "btnRelease";
+let id_btn_label = "lblMessage";
 
 let last_det_time = null;
 let current_time = null;
+let current_status = 0;
 let is_light_on = false;
+let form_interaction = false;
 let first_update = true;
+let applyPressed = false;
+let system_time_ctrl_obtained = 0
+let control_lock_time_mins = 60 * 1000;
 let clock = 1000;
 
 let time_scheme = [];
 for (var i = 0; i < 24; i++) {
 	time_scheme.push(0);
+}
+
+function updateStatus(status) {
+	if (status == null) return;
+	current_status = status;
+	switch(current_status) {
+		case status_auto:
+			document.getElementById(id_status).innerHTML = "Status: <b>AUTO</b>";
+			system_time_ctrl_obtained = 0;
+			document.getElementById(id_btn_release).disabled = true;
+			document.getElementById(id_button).disabled = false;
+			break;
+		case status_dash:
+			document.getElementById(id_status).innerHTML = "Status: <b>DASHBOARD CONTROL</b>";
+			document.getElementById(id_btn_release).disabled = false;
+			document.getElementById(id_button).disabled = false;
+			if (applyPressed) {
+				system_time_ctrl_obtained = Date.now();
+				applyPressed = false;
+			}
+			break;
+		case status_mobile:
+			document.getElementById(id_status).innerHTML = "Status: <b>MOBILE APP CONTROL</b>";
+			document.getElementById(id_btn_release).disabled = true;
+			document.getElementById(id_button).disabled = true;
+			system_time_ctrl_obtained = 0;
+			break;
+		default:
+			document.getElementById(id_status).innerHTML = "Status: <b>Unknown</b>";
+			document.getElementById(id_btn_release).disabled = true;
+			document.getElementById(id_button).disabled = true;
+			system_time_ctrl_obtained = 0;
+			break;
+	}
 }
 
 function updateRollerBlind(value) {
@@ -46,10 +94,16 @@ function updateLight(is_on) {
 		document.getElementById(id_led).style.backgroundColor = "#ABFF00";
 		document.getElementById(id_led).style.boxShadow = "rgba(0, 0, 0, 0.2) 0 -1px 7px 1px, inset #304701 0 -1px 9px, #89FF00 0 2px 12px";
 		is_light_on = true;
+		if (!form_interaction) {
+			document.getElementById(id_switch).checked = true;
+		}
 	} else {
 		document.getElementById(id_led).style.backgroundColor = "#86C900";
 		document.getElementById(id_led).style.boxShadow = "none";
 		is_light_on = false;
+		if (!form_interaction) {
+			document.getElementById(id_switch).checked = false;
+		}
 	}
 }
 	
@@ -77,14 +131,6 @@ function updateTime(hour, min) {
 	}
 }
 
-function diff_minutes(dt2, dt1) {
-	if (dt2 == null || dt1 == null) return 0;
-	
-	var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-	diff /= 60;
-	return Math.abs(Math.round(diff));  
-}
-
 function updateGraph() {
 	if (!first_update && is_light_on) {
 		var mins_passed = diff_minutes(current_time,last_det_time);
@@ -109,18 +155,79 @@ function updateGraph() {
 	}
 }
 
+function formOnClick() {
+	form_interaction = true;
+}
+
+function applyOnClick() {
+	var confirmation_msg_reset = 3 * 1000;
+	var roll = document.getElementById(id_roll).value;
+	var light = 0;
+	if (document.getElementById(id_switch).checked) {
+		light++;
+	}
+	document.getElementById(id_button).disabled = true;
+	document.getElementById(id_btn_release).disabled = true;
+	document.getElementById(id_btn_label).innerHTML = "Waiting for server response...";
+	form_interaction = false;
+	applyPressed = true;
+	
+	var req = new XMLHttpRequest();
+	req.onreadystatechange(function(){
+		document.getElementById(id_button).disabled = false;
+		document.getElementById(id_btn_release).disabled = false;
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(id_btn_label).innerHTML = this.responseText;
+		} else {
+			document.getElementById(id_btn_label).innerHTML = "An error occured while sending the request.";
+		}
+		setTimeout(function(){
+			document.getElementById(id_btn_label).innerHTML = "";
+		}, confirmation_msg_reset);
+	});
+	req.open("POST", "accessControl", true);
+	req.send("status="+status_dash+"&light="+light+"&roll="+roll);
+}
+
+function releaseOnClick() {
+	if (current_status == status_dash) {
+		system_time_ctrl_obtained = 1;
+	}
+}
+
+function releaseControlLock() {
+	var req = new XMLHttpRequest();
+	req.open("POST", "accessControl", true);
+	req.send("status="+status_auto+"&light=0&roll=0");
+}
+
+function diff_minutes(dt2, dt1) {
+	if (dt2 == null || dt1 == null) return 0;
+	
+	var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+	diff /= 60;
+	return Math.abs(Math.round(diff));  
+}
+
 function getDataFromServer() {
-	let req = new XMLHttpRequest();
+	var req = new XMLHttpRequest();
 	req.onload = function() {
-		document.getElementById(id_status).innerHTML = "Status: " + this.responseText.split(";")[0];
 		updateGraph();
+		updateStatus(this.responseText.split(";")[0]);
 		updateTime(this.responseText.split(";")[1], this.responseText.split(";")[2]);
 		updateLight(this.responseText.split(";")[3]);
 		updateRollerBlind(this.responseText.split(";")[4]);
 		updateLightLvl(this.responseText.split(";")[5]);
 		first_update = false;
 	};
-	req.open("GET", "requestTelemetry", true);
+	req.open("GET", "requestData", true);
     req.send();
-	setTimeout(getDataFromServer, clock);
+}
+
+function runtime() {
+	getDataFromServer();
+	if (system_time_ctrl_obtained > 0 && (Date.now()-system_time_ctrl_obtained) >= control_lock_time) {
+		releaseControlLock();
+	}
+	setTimeout(runtime, clock);
 }
