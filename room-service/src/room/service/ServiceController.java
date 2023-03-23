316ff0c80fcd78252ed35e008faca9ecd208b599
DanielPellanda <pellanda.daniel01@gmail.com>
2023-03-23 19:38:05 +0100

    Adjusted ServiceController

--- room-service/src/room/service/ServiceController.java
+++ room-service/src/room/service/ServiceController.java
@@ -26,8 +26,9 @@ public class ServiceController implements Service{
             "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
             "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 	
-	private final int espPort = 9000;
-	private final int dashboardPort = 9001;
+	private final int espPort = 5067;
+	private final int dashboardPort = 8120;
+	private final String networkHost = "wlan";
 	
 	private HttpChannel espConnector;
 	private HttpChannel dashboardConnector;
@@ -44,8 +45,8 @@ public class ServiceController implements Service{
 			arduinoConnector = new SerialChannel();
 			
 			espConnector.addHandler("/updateData", req -> data.updateEspData(req.getPostData()));
-			dashboardConnector.addHandler("/requestData", req -> data.getData());
-			dashboardConnector.addHandler("/accessControl", req -> data.putAccessRequest(req.getPostData()));
+			dashboardConnector.addHandler("/requestData", req -> data.getDashboardData());
+			dashboardConnector.addHandler("/accessControl", req -> data.putRequest(req.getPostData()));
 		} catch (SerialPortException e) {
 			System.err.println(e.toString());
 			System.exit(25);
@@ -70,10 +71,7 @@ public class ServiceController implements Service{
 			arduinoConnector.start();
 			while(true) {
 				synchronized (data) {
-					if (data.hasRequest()) {
-						arduinoConnector.send(data.getRequestDescription());
-					}
-					arduinoConnector.send(data.personDetected + ";" + data.lightLevel);
+					arduinoConnector.send(data.getArduinoData());
 					data.updateArduinoData(arduinoConnector.read());
 					Thread.sleep(period);
 				}
@@ -99,7 +97,7 @@ public class ServiceController implements Service{
 	private InetAddress getWlanInterfaceAddress() throws UnknownHostException, SocketException {
 		final Pattern ipv4Pattern = Pattern.compile(IPV4_REGEX);
 		for (NetworkInterface ie : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-			if (ie.getName().contains("wlan")) {
+			if (ie.getName().contains(networkHost)) {
 				for (InetAddress addr : Collections.list(ie.getInetAddresses())) {
 					if (ipv4Pattern.matcher(addr.getHostAddress()).matches()) {
 						return addr;
@@ -111,14 +109,19 @@ public class ServiceController implements Service{
 	}
 	
 	private class Database {
-		public Optional<String> request = Optional.empty();
-		public boolean personDetected = false;
-		public boolean lightOn = false;
-		public int lightLevel = 0;
-		public int status = 0;
-		public int rollerBlind = 0;
-		public int hours = 0;
-		public int mins = 0;
+		private class Request {
+			public int status = 0;
+			public int rollerBlind = 0;
+			public boolean light = false;
+		}
+		private Request request = new Request();
+		private boolean personDetected = false;
+		private boolean lightOn = false;
+		private int lightLevel = 0;
+		private int status = 0;
+		private int rollerBlind = 0;
+		private int hours = 0;
+		private int mins = 0;
 		
 		public HttpResponse updateEspData(final Optional<String> input) {
 			if (input.isEmpty()) {
@@ -133,7 +136,7 @@ public class ServiceController implements Service{
 			}
 			
 			synchronized(this) {
-				personDetected = parameters[0].compareTo("1") == 0 ? true : false;
+				personDetected = parameters[0].compareTo("1") == 0;
 				lightLevel = Integer.parseInt(parameters[1]);
 			}
 			
@@ -156,48 +159,35 @@ public class ServiceController implements Service{
 			}
 		}
 		
-		private HttpResponse putAccessRequest(final Optional<String> postData) {
+		private HttpResponse putRequest(final Optional<String> postData) {
 			if (postData.isEmpty()) {
 				final String response = "Java Error: post data is empty";
 				return new HttpResponse(400, response.length(), response);
 			}
 			final Map<String, String> postArgs = new HashMap<>();
 			HttpRequestObject.parseQuery(postArgs, postData.get());
-			String request = "";
-			for (Map.Entry<String, String> entry : postArgs.entrySet()) {
-				request += entry.getValue() + ";";
-			}
-			request = request.substring(0, request.length()-2);
 			
 			synchronized(this) {
-				this.request = Optional.of(request);
+				 if (postArgs.containsKey("status")) {
+					 this.request.status = Integer.parseInt(postArgs.get("status"));
+				 }
+				 if (postArgs.containsKey("light")) {
+					 this.request.light = postArgs.get("light").compareTo("1") == 0;
+				 }
+				 if (postArgs.containsKey("roll")) {
+					 this.request.rollerBlind = Integer.parseInt(postArgs.get("roll"));
+				 }
 			}
 			
 			final String response = "Settings successfully applied.";
 			return new HttpResponse(200, response.length(), response);
 		}
 		
-		public boolean hasRequest() {
-			boolean result = false;
-			synchronized(this) {
-				result = request.isPresent();
-			}
-			return result;
+		private String getArduinoData() {
+			return request.status + ";" + request.light + ";" + request.rollerBlind + ";" + personDetected + ";" + lightLevel;
 		}
 		
-		public String getRequestDescription() {
-			String req = "";
-			synchronized(this) {
-				if (request.isEmpty()) {
-					req = "";
-				}
-				req = request.get();
-				request = Optional.empty();
-			}
-			return req;
-		}
-		
-		private HttpResponse getData() {
+		private HttpResponse getDashboardData() {
 			final String response = data.status + ";" + data.hours + ";" + data.mins + ";" + (data.lightOn ? "1" : "0") + ";" + data.rollerBlind;
 			return new HttpResponse(200, response.length(), response);
 		}
