b5276c2f623ba20daa16aff9474e13ab885bc5fe
DanielPellanda <pellanda.daniel01@gmail.com>
2023-04-03 21:42:56 +0200

    Fixed some errors and warnings

--- room-app/app/src/main/java/room/app/activity/MainActivity.java
+++ room-app/app/src/main/java/room/app/activity/MainActivity.java
@@ -1,25 +1,17 @@
 package room.app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import android.os.Bundle;
 
-import room.app.R;
 import room.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //setSupportActionBar(binding.toolbar);
+        setSupportActionBar(binding.toolbar);
     }
 }
