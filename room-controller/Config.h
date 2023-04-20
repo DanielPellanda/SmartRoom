7fb8b77e92672038c687cdb8e5004a3a0c22df15
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2023-04-20 19:11:09 +0200

    fixed bluetooth communication

--- room-controller/Config.h
+++ room-controller/Config.h
@@ -1,5 +1,5 @@
 #define BASE_PERIOD 100
 #define LED_PIN 8
 #define SERVO_PIN 7
-#define BT_RX_PIN 3
-#define BT_TX_PIN 2
+#define BT_RX_PIN 2 // to be connected to TX of the BT module
+#define BT_TX_PIN 3 // to be connected to RX of the BT module
